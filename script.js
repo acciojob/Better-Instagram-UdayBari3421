c1d07957be3c3f0b45d39df494ae45d0f2b78c0e
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-26 18:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.getElementById("parent");
-  const images = container.getElementsByClassName("image");
+const container = document.getElementById("parent");
+const divs = container.getElementsByTagName("div");
 
-  let dragged;
+function onDragStart(event) {
+  event.dataTransfer.setData("sourceId", event.target.id);
+}
 
-  function onDragStart(event) {
-    event.dataTransfer.setData("text/plain", event.target.id);
-    dragged = event.target;
-  }
+function onDragOver(event) {
+  event.preventDefault();
+}
 
-  function onDragOver(event) {
-    event.preventDefault();
-  }
+function onDrop(event) {
+  const sourceId = event.dataTransfer.getData("sourceId");
+  const sourceElement = document.getElementById(sourceId);
+  const destElement = event.target;
 
-  function onDrop(event) {
-    event.preventDefault();
-    const sourceId = event.dataTransfer.getData("text/plain");
-    const sourceElement = document.getElementById(sourceId);
-    const destElement = event.target;
+  const sourceNextElement = sourceElement.nextElementSibling;
+  const destNextElement = destElement.nextElementSibling;
 
-    // Ensure that source and destination elements exist before accessing properties
-    if (sourceElement && destElement && destElement.classList.contains("image")) {
-      // Swap the background images
-      const temp = sourceElement.style.backgroundImage;
-      sourceElement.style.backgroundImage = destElement.style.backgroundImage;
-      destElement.style.backgroundImage = temp;
-    }
-  }
+  container.insertBefore(destElement, sourceNextElement);
 
-  for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener("dragstart", onDragStart);
-    images[i].addEventListener("dragover", onDragOver);
-    images[i].addEventListener("drop", onDrop);
-  }
-});
+  container.insertBefore(sourceElement, destNextElement);
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener("dragstart", onDragStart);
+  divs[i].addEventListener("dragover", onDragOver);
+  divs[i].addEventListener("drop", onDrop);
+}
